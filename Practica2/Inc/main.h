9e96988c38681cb0beec318998cf62d004fc60c3
alejandro virgillo <alevirgillo_95@hotmail.com>
2021-11-10 21:35:29 -0300

    Agregado de practica 3

---
+++ Practica2/Inc/main.h
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * @file    main.h
+  * @author  AV
+  * @brief   archivo header main del practico 1, implementacion de delays no bloqueantes
+  *****************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/* Includes ------------------------------------------------------------------*/
+
+#include <stdio.h>
+#include "stm32f4xx_hal.h"  		/* <- HAL include */
+#include "stm32f4xx_nucleo_144.h" 	/* <- BSP include */
+
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+#endif /* __MAIN_H */
